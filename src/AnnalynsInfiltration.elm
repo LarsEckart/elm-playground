64ebca176c64d377626448ad05dd344bf1c7e74d
LarsEckart <lars.eckart@hey.com>
2022-03-05 10:05:50 +0200

    exorsim track, booleans

---
+++ src/AnnalynsInfiltration.elm
@@ -0,0 +1,42 @@
+module AnnalynsInfiltration exposing (canFastAttack, canFreePrisoner, canSignalPrisoner, canSpy, stealthAttackDamage)
+
+
+canFastAttack : Bool -> Bool
+canFastAttack knightIsAwake =
+    knightIsAwake == False
+
+
+canSpy : Bool -> Bool -> Bool -> Bool
+canSpy knightIsAwake archerIsAwake prisonerIsAwake =
+    xor knightIsAwake archerIsAwake
+        || xor knightIsAwake prisonerIsAwake
+        || xor archerIsAwake prisonerIsAwake
+        || knightIsAwake
+        && archerIsAwake
+        && prisonerIsAwake
+
+
+canSignalPrisoner : Bool -> Bool -> Bool
+canSignalPrisoner archerIsAwake prisonerIsAwake =
+    not archerIsAwake && prisonerIsAwake
+
+
+canFreePrisoner : Bool -> Bool -> Bool -> Bool -> Bool
+canFreePrisoner knightIsAwake archerIsAwake prisonerIsAwake petDogIsPresent =
+    (petDogIsPresent
+        && not archerIsAwake
+    )
+        || (not petDogIsPresent
+                && not archerIsAwake
+                && not knightIsAwake
+                && prisonerIsAwake
+           )
+
+
+stealthAttackDamage : Bool -> Int
+stealthAttackDamage annalynIsDetected =
+    if annalynIsDetected then
+        7
+
+    else
+        12
