2ba661a860f0a7d6ef589348569323f38af277dc
LarsEckart <lars.eckart@hey.com>
2022-03-07 09:03:27 +0200

    exercism, elm, Let

--- tests/Tests.elm
+++ tests/Tests.elm
@@ -4,6 +4,7 @@ import AnnalynsInfiltration exposing (canFastAttack, canFreePrisoner, canSignalP
 import Bandwagoner exposing (..)
 import BettysBikeShop exposing (..)
 import Expect exposing (FloatingPointTolerance(..))
+import LuigisLusciousLasagna exposing (remainingTimeInMinutes)
 import Test exposing (..)
 import TracksOnTracksOnTracks exposing (..)
 import ValentinesDay exposing (..)
@@ -705,4 +706,14 @@ tests =
             \_ ->
                 rateActivity (Restaurant Turkish)
                     |> Expect.equal Maybe
+
+        -- lasange
+        , test "For a 3-layers lasagna started 10 minutes ago, there are 36 minutes remaining" <|
+            \_ ->
+                remainingTimeInMinutes 3 10
+                    |> Expect.equal ((3 * 2) + 40 - 10)
+        , test "For a 6-layers lasagna started 30 minutes ago, there are 22 minutes remaining" <|
+            \_ ->
+                remainingTimeInMinutes 6 30
+                    |> Expect.equal ((6 * 2) + 40 - 30)
         ]
