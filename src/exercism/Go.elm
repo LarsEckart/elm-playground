02ab9573f4d833120d3a7e3b83ec1b96722fcd7d
Lars Eckart <lars.eckart@hey.com>
2024-02-18 17:27:03 +0200

    reset project to make room for dorfromantik calculator

--- src/exercism/Go.elm
+++
@@ -1,8 +0,0 @@
-module Go exposing (..)
-
-import GoSupport exposing (..)
-
-
-applyRules : Game -> Rule -> NonValidatingRule -> Rule -> Rule -> Game
-applyRules game oneStonePerPointRule captureRule libertyRule koRule =
-    changePlayer game
