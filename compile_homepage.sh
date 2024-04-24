ac05d746df0cfb91475a25603404ae5bca85b8fe
Lars Eckart <lars.eckart@hey.com>
2024-04-24 22:04:13 +0200

    minify

--- compile_homepage.sh
+++ compile_homepage.sh
@@ -1,4 +1,14 @@
-# will compile src/HomePage.elm to index.html
-elm make src/Main.elm --output elm.js
-mkdir dist
-cp public/index.html dist/index.html
+#!/bin/sh
+
+set -e
+
+js="elm.js"
+min="elm.min.js"
+
+elm make --optimize --output=$js $@
+
+uglifyjs $js --compress "pure_funcs=[F2,F3,F4,F5,F6,F7,F8,F9,A2,A3,A4,A5,A6,A7,A8,A9],pure_getters,keep_fargs=false,unsafe_comps,unsafe" | uglifyjs --mangle --output $min
+
+echo "Initial size: $(cat $js | wc -c) bytes  ($js)"
+echo "Minified size:$(cat $min | wc -c) bytes  ($min)"
+echo "Gzipped size: $(cat $min | gzip -c | wc -c) bytes"
